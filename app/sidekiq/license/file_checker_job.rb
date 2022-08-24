43ac66d1d1177884973240886c125a1956466be0
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-08-24 12:03:59 +0200

    Implementing mail notification of license file ingestion completion

--- app/sidekiq/license/file_checker_job.rb
+++ app/sidekiq/license/file_checker_job.rb
@@ -17,6 +17,18 @@ class License::FileCheckerJob
 
     if @not_found.nil?
       ingestion_file.mark_as_processed
+
+      ::License::IngestionMailer
+        .with(user: ingestion_file.uploader)
+        .file_processed
+        .deliver_later
+
+      User.where(role: :owner).each do |user|
+        ::License::IngestionMailer
+          .with(user: user, uploader: ingestion_file.uploader)
+          .file_processed
+          .deliver_later
+      end
     else
       ::License::Scheduler.check_file_ingestion(ingestion_file.id)
     end
