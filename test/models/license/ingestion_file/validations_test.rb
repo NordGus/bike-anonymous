62adf595fecb5cebb73187f459fc36afb4792bf3
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-09-01 12:10:11 +0200

    User validations test corrections

--- test/models/license/ingestion_file/validations_test.rb
+++ test/models/license/ingestion_file/validations_test.rb
@@ -2,19 +2,41 @@ require "test_helper"
 
 class License::IngestionFile::ValidationsTest < ActiveSupport::TestCase
   test "valid ingestion file" do
-    skip "can't find a way to make model valid"
-    file = File.open(Rails.root.join("test/fixtures/files/license/ingestion_file.csv"))
-
-    ingestion_file = ::License::IngestionFile.new(file: file)
+    ingestion_file = ::License::IngestionFile.new(
+      file: {
+        io: file,
+        filename: "ingestion_file.csv"
+      },
+      uploader: users(:partner)
+    )
 
     assert ingestion_file.valid?
   end
 
   test "invalid ingestion file because of not uploaded file" do
-    ingestion_file = ::License::IngestionFile.new
+    ingestion_file = ::License::IngestionFile.new(
+      uploader: users(:partner)
+    )
 
     assert_not ingestion_file.valid?
     assert_equal [{error: :blank}], ingestion_file.errors.details[:file]
+  end
+
+  test "invalid ingestion file because uploader is blank" do
+    ingestion_file = ::License::IngestionFile.new(
+      file: {
+        io: file,
+        filename: "ingestion_file.csv"
+      }
+    )
+
+    assert_not ingestion_file.valid?
     assert_equal [{error: :blank}], ingestion_file.errors.details[:uploader]
+  end
+
+  private
+
+  def file
+    @file ||= File.open(Rails.root.join("test", "fixtures", "files", "license", "ingestion_file.csv"))
   end
 end
