74e44f70390c3140218224b6da5bde62931f1a90
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-09-02 13:52:56 +0200

    CyclistMailer tests

---
+++ test/mailers/previews/license/cyclist_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/license/ingestion_mailer
+class License::CyclistMailerPreview < ActionMailer::Preview
+  def new_license
+    
+  end
+end
